a9ca06d4e7b1e7e44ad2f5a3c1f9a3b5ad7625b0
windywany <windywany@163.com>
2016-12-27 18:28:23 +0800

    添加框架相关的加载器

---
+++ src/wula/cms/CmfConfigurationLoader.php
@@ -0,0 +1,24 @@
+<?php
+/*
+ * 内容管理框架配置加载器.
+ *
+ * (c) Leo Ning <windywany@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace wula\cms;
+
+use wulaphp\conf\ConfigurationLoader;
+
+class CmfConfigurationLoader extends ConfigurationLoader {
+	public function __construct() {
+		//检测是否安装.
+		if (is_file(CONFIG_PATH . 'install.lock')) {
+			define('WULACMF_INSTALLED', true);
+		} else {
+			define('WULACMF_INSTALLED', false);
+		}
+	}
+}
---
+++ src/wula/cms/CmfModuleLoader.php
@@ -0,0 +1,32 @@
+<?php
+/*
+ * 模块加载器.
+ *
+ * This file is part of wulacms.
+ *
+ * (c) Leo Ning <windywany@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace wula\cms;
+
+use wulaphp\app\Module;
+use wulaphp\app\ModuleLoader;
+
+class CmfModuleLoader extends ModuleLoader {
+
+	public function isEnabled(Module $module) {
+		if (WULACMF_INSTALLED) {
+			// TODO: 此处需要从数据进行检验
+			return true;
+		}
+		$name = $module->getNamespace();
+		if ($name == 'core') {
+			return true;
+		}
+
+		return false;
+	}
+}
