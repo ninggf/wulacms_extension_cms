31c4e1e45695134178186e26f75e7d9a605db1db
windywany <windywany@163.com>
2018-09-14 15:44:10 +0800

    fix loadConfig bug

--- src/wula/cms/CmfConfigurationLoader.php
+++ src/wula/cms/CmfConfigurationLoader.php
@@ -19,105 +19,112 @@ use wulaphp\io\Request;
 use wulaphp\router\Router;
 
 class CmfConfigurationLoader extends ConfigurationLoader {
-	public function __construct() {
-		//检测是否安装.
-		if (is_file(CONFIG_PATH . 'install.lock')) {
-			define('WULACMF_INSTALLED', true);
-		} else {
-			define('WULACMF_INSTALLED', false);
-			if (!defined('WULACMF_WEB_INSTALLER')) {
-				bind('artisan\getCommands', function ($cmds) {
-					$cmds['wulacms:install'] = new InstallCommand();
+    public function __construct() {
+        //检测是否安装.
+        if (is_file(CONFIG_PATH . 'install.lock')) {
+            define('WULACMF_INSTALLED', true);
+        } else {
+            define('WULACMF_INSTALLED', false);
+            if (!defined('WULACMF_WEB_INSTALLER')) {
+                bind('artisan\getCommands', function ($cmds) {
+                    $cmds['wulacms:install'] = new InstallCommand();
 
-					return $cmds;
-				});
-			}
-		}
-	}
+                    return $cmds;
+                });
+            }
+        }
+    }
 
-	/**
-	 * @param string $name
-	 *
-	 * @return mixed|\wulaphp\conf\Configuration
-	 */
-	public function loadConfig($name = 'default') {
-		//优先从文件加载
-		$config = parent::loadConfig($name);
-		if (WULACMF_INSTALLED) {
-			//从缓存加载
-			$setting = RtCache::get('cfg.' . $name);
-			if (!is_array($setting)) {
-				//从数据库加载
-				try {
-					$setting = App::table('settings')->findAll(['group' => $name], 'name,value')->toArray('value', 'name');
-					RtCache::add('cfg.' . $name, $setting);
-				} catch (\Exception $e) {
-					log_warn($e->getMessage());//无法连接数据库
-				}
-			}
-			if ($setting) {
-				$config->setConfigs($setting);
-			}
-		}
+    /**
+     * @param string $name
+     *
+     * @return mixed|\wulaphp\conf\Configuration
+     */
+    public function loadConfig($name = 'default') {
+        //优先从文件加载
+        $config = parent::loadConfig($name);
+        if (WULACMF_INSTALLED) {
+            if ($name = 'default' && !defined('DEBUG')) {
+                $debug = $config->get('debug', DEBUG_ERROR);
+                if ($debug > 1000 || $debug < 0) {
+                    $debug = DEBUG_OFF;
+                }
+                define('DEBUG', $debug);
+            }
+            //从缓存加载
+            $setting = RtCache::get('cfg.' . $name);
+            if (!is_array($setting)) {
+                //从数据库加载
+                try {
+                    $setting = App::table('settings')->findAll(['group' => $name], 'name,value')->toArray('value', 'name');
+                    RtCache::add('cfg.' . $name, $setting);
+                } catch (\Exception $e) {
+                    log_warn($e->getMessage());//无法连接数据库
+                }
+            }
+            if ($setting) {
+                $config->setConfigs($setting);
+            }
+        }
 
-		return $config;
-	}
+        return $config;
+    }
 
-	/**
-	 * 加载配置前运行CMS特性。
-	 */
-	public function beforeLoad() {
-		if (defined('ANTI_CC') && ANTI_CC) {
-			CmsFeatureManager::register(new LimitFeature());
-		}
-		if (APP_MODE == 'pro') {//只有线上才开启缓存功能
-			CmsFeatureManager::register(new CacheFeature());
-		}
-		$features = CmsFeatureManager::getFeatures();
-		if ($features) {
-			ksort($features);
-			$rst = [];
-			$url = Router::getFullURI();
-			foreach ($features as $fs) {
-				/**@var \wula\cms\ICmsFeature $f */
-				foreach ($fs as $f) {
-					$rst[] = $f->perform($url) === false ? 0 : 1;
-				}
-			}
+    /**
+     * 加载配置前运行CMS特性。
+     */
+    public function beforeLoad() {
+        if (defined('ANTI_CC') && ANTI_CC) {
+            CmsFeatureManager::register(new LimitFeature());
+        }
+        if (APP_MODE == 'pro') {//只有线上才开启缓存功能
+            CmsFeatureManager::register(new CacheFeature());
+        }
+        $features = CmsFeatureManager::getFeatures();
+        if ($features) {
+            ksort($features);
+            $rst = [];
+            $url = Router::getFullURI();
+            foreach ($features as $fs) {
+                /**@var \wula\cms\ICmsFeature $f */
+                foreach ($fs as $f) {
+                    $rst[] = $f->perform($url) === false ? 0 : 1;
+                }
+            }
 
-			if ($rst && !array_product($rst)) {//有特性要求停止运行（返回了false）
-				status_header(403);
-				exit();
-			}
-		}
-	}
+            if ($rst && !array_product($rst)) {//有特性要求停止运行（返回了false）
+                status_header(403);
+                exit();
+            }
+        }
+    }
 
-	public function postLoad() {
-		$ip = Request::getIp();
-		if ($ip && App::bcfg('offline')) {
-			$ips = trim(App::cfg('allowedIps'));
-			$msg = App::cfg('offlineMsg', 'Service Unavailable');
-			if (empty($ips)) {
-				$this->httpout(503, $msg);
-			}
-			$ips = explode("\n", $ips);
-			if (!in_array($ip, $ips)) {
-				$this->httpout(503, $msg);
-			}
-		}
-	}
+    public function postLoad() {
+        $ip = Request::getIp();
+        if ($ip && App::bcfg('offline')) {
+            $ips = trim(App::cfg('allowedIps'));
+            $msg = App::cfg('offlineMsg', 'Service Unavailable');
+            if (empty($ips)) {
+                $this->httpout(503, $msg);
+            }
+            $ips = explode("\n", $ips);
+            if (!in_array($ip, $ips)) {
+                $this->httpout(503, $msg);
+            }
+        }
+    }
 
-	/**
-	 * 输出http响应输出。
-	 *
-	 * @param string|int $status 状态
-	 * @param string     $message
-	 */
-	private function httpout($status, $message = '') {
-		status_header($status);
-		if ($message) {
-			echo $message;
-		}
-		exit();
-	}
+    /**
+     * 输出http响应输出。
+     *
+     * @param string|int $status 状态
+     * @param string     $message
+     */
+    private function httpout($status, $message = '') {
+        status_header($status);
+        if ($message) {
+            echo $message;
+        }
+        exit();
+    }
 }
