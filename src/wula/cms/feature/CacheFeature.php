291174cc3d1a96ebb09d2b9ae432847fd853e7fc
windywany <windywany@163.com>
2019-02-26 16:08:11 +0800

    优化缓存特性

--- src/wula/cms/feature/CacheFeature.php
+++ src/wula/cms/feature/CacheFeature.php
@@ -21,104 +21,92 @@ use wulaphp\util\RedisLock;
  * @package wula\cms\feature
  */
 class CacheFeature implements ICmsFeature {
-	public function getPriority() {
-		return 100;
-	}
+    public function getPriority() {
+        return 100;
+    }
 
-	public function getId() {
-		return 'cache';
-	}
+    public function getId() {
+        return 'cache';
+    }
 
-	public function perform($url) {
-		if (APP_MODE == 'pro') {//只有线上才开启缓存功能
-			$cacher = Cache::getCache();
-			$domain = $_SERVER ['HTTP_HOST'];
-			$qstr   = get_query_string();//参数
-			$cid    = md5($domain . $url . $qstr);
-			$page   = $cacher->get($cid);
-			//防雪崩机制: 加锁读缓存
-			if (!$page && defined('ANTI_AVALANCHE') && ANTI_AVALANCHE) {
-				$wait = false;
-				RedisLock::ulock($cid, 20, $wait);
-				if ($wait) {//被锁，说明有其它人会更新缓存 ，再读一次
-					$page = $cacher->get($cid);
-				}
-			}
-			//缓存命中
-			if ($page && is_array($page)) {
-				if (isset($wait)) {
-					RedisLock::uunlock($cid);
-				}
-				if (@ob_get_status()) {
-					@ob_end_clean();
-				}
-				@ob_start();
-				if (defined('GZIP_ENABLED') && GZIP_ENABLED && extension_loaded("zlib")) {
-					$gzip = @ini_get('zlib.output_compression');
-					if (!$gzip) {
-						@ini_set('zlib.output_compression', 1);
-					}
-					@ini_set('zlib.output_compression_level', 5);
-				} else {
-					@ini_set('zlib.output_compression', 0);
-					@ini_set('zlib.output_compression_level', -1);
-				}
-				$page = apply_filter('alter_page_cache', $page);
-				@list($content, $headers, $time, $expire) = $page;
-				if (isset ($_SERVER ['HTTP_IF_MODIFIED_SINCE']) && strtotime($_SERVER ['HTTP_IF_MODIFIED_SINCE']) === $time) {
-					$protocol      = $_SERVER ["SERVER_PROTOCOL"];
-					$status_header = "$protocol 304 Not Modified";
-					Response::cache($expire, $time);
-					@header($status_header, true, 304);
-					if (php_sapi_name() == 'cgi-fcgi') {
-						@header('Status: 304 Not Modified');
-					}
-				} else {
-					if ($headers) {
-						foreach ($headers as $h => $v) {
-							@header($h . ': ', $v);
-						}
-					}
-					if ($time !== 0) {
-						Response::cache($expire, $time);
-					} else if ($time == 0) {
-						Response::nocache();
-					}
-					echo $content;
-				}
-				exit ();
-			} else {
-				//注册缓存内容处理器
-				$unlock = isset($wait);
-				bind('before_output_content', function ($content, View $view) use ($cid, $cacher, $unlock, $url) {
-					//需要缓存
-					try {
-						if (defined('CACHE_EXPIRE') && CACHE_EXPIRE > 0) {
-							//插件或扩展可以将最后修改时间设为0来取消本次缓存.
-							$time = apply_filter('alter_page_modified_time', time());
-							$cacher->add($cid, [
-								$content,//缓存内容
-								$view->getHeaders(),//原输出头
-								$time == 0 ? time() : $time,// 最后修改时间
-								CACHE_EXPIRE//缓存时间
-							], CACHE_EXPIRE);
-							fire('on_page_cached', $cid, $url);
-							if ($time > 0) {
-								Response::cache(CACHE_EXPIRE, $time);
-							} else if ($time == 0) {
-								Response::nocache();
-							}
-						}
-					} catch (\Exception $e) {
+    public function perform($url) {
+        if (APP_MODE == 'pro') {//只有线上才开启缓存功能
+            $cacher = Cache::getCache();
+            $domain = $_SERVER ['HTTP_HOST'];
+            $qstr   = get_query_string();//参数
+            $cid    = md5($domain . $url . $qstr);
+            $page   = $cacher->get($cid);
+            //防雪崩机制: 加锁读缓存
+            if (!$page && defined('ANTI_AVALANCHE') && ANTI_AVALANCHE) {
+                $wait = false;
+                RedisLock::ulock($cid, 20, $wait);
+                if ($wait) {//被锁，说明有其它人会更新缓存 ，再读一次
+                    $page = $cacher->get($cid);
+                }
+            }
+            //缓存命中
+            if ($page && is_array($page)) {
+                if (isset($wait)) {
+                    RedisLock::uunlock($cid);
+                }
+                $page = apply_filter('alter_page_cache', $page);
+                @list($content, $headers, $time, $expire) = $page;
+                if (isset ($_SERVER ['HTTP_IF_MODIFIED_SINCE']) && strtotime($_SERVER ['HTTP_IF_MODIFIED_SINCE']) === $time) {
+                    http_response_code(304);
+                    if (php_sapi_name() == 'cgi-fcgi') {
+                        @header('Status: 304 Not Modified');
+                    }
+                } else {
+                    if ($headers) {
+                        foreach ($headers as $h => $v) {
+                            @header($h . ': ', $v);
+                        }
+                    }
+                    if ($time !== 0) {
+                        Response::cache($expire, $time);
+                    } else if ($time == 0) {
+                        Response::nocache();
+                    }
+                    echo $content;
+                }
+                exit ();
+            } else {
+                //注册缓存内容处理器
+                $unlock = isset($wait);
+                bind('before_output_content', function ($content, View $view) use ($cid, $cacher, $unlock, $url) {
+                    //需要缓存
+                    if (defined('CACHE_EXPIRE') && CACHE_EXPIRE > 0) {
+                        try {
+                            //插件或扩展可以将最后修改时间设为0来取消本次缓存.
+                            $time    = apply_filter('alter_page_modified_time', time());
+                            $headers = $view->getHeaders();//原输出头
+                            //$headers['ETag'] = $cid;
+                            $cacher->add($cid, [
+                                $content,//缓存内容
+                                $headers,
+                                $time == 0 ? time() : $time,// 最后修改时间
+                                CACHE_EXPIRE//缓存时间
+                            ], CACHE_EXPIRE);
+                            try {
+                                fire('on_page_cached', $cid, $url);
+                            } catch (\Exception $ee) {
+                            }
+                            if ($time > 0) {
+                                Response::cache(CACHE_EXPIRE, $time);
+                            } else if ($time == 0) {
+                                Response::nocache();
+                            }
+                        } catch (\Exception $e) {
+                        }
+                    }
 
-					}
-					if ($unlock) {
-						RedisLock::uunlock($cid);
-					}
+                    if ($unlock) {
+                        RedisLock::uunlock($cid);
+                    }
 
-					return $content;
-				}, 100, 2);
-			}
-		}
-	}
+                    return $content;
+                }, 100, 2);
+            }
+        }
+    }
 }
